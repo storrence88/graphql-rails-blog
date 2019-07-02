185890b935669cd060b136c214af7e12fa95d806
Steven Torrence <storrence88@gmail.com>
2019-07-01 22:10:33 -0500

    Add Faker data to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    password { "" }
+    name { Faker::Internet.username(separators = %w(._-)) }
+    password { Faker::Internet.password(6, 24) }
   end
 end
